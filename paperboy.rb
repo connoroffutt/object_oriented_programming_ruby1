3b18aba0a7c62b4c7013f589e39b354a68445af4
connor <connor.ferguson.offutt@gmail.com>
2016-10-20 15:59:35 -0400

    added deliver equation, added houses variable and final report.

--- paperboy.rb
+++ paperboy.rb
@@ -3,18 +3,46 @@ class Paperboy
 attr_accessor :name, :experience
 attr_reader :earnings
 
-  def initialize(name, experience, earnings)
+  def initialize(name)
     @name = name
-    @experience = experience
-    @earnings = earnings
+    @experience = 0
+    @earnings = 0
   end
 
   def quota
     (@experience/2) + 50
   end
+
+  def deliver(start_address, end_address)
+       houses = (end_address) - (start_address)
+     if houses < quota
+       wage = 0.25 * houses - 2
+     else
+       wage = 0.25 * houses + (houses - quota) * (0.25)
+     end
+
+     @experience += houses
+     @earnings += wage
+     
+  end
+
+
+
+  def report
+    puts "I'm #{@name} and i've delivered #{@experience} papers! I've earned #{@earnings} so far!"
+  end
+
 end
 
-johnny = Paperboy.new('Johnny', 50, 1/4)
-timmy = Paperboy.new('Timmy', 100, 1/4)
 
-puts johnny.quota
+
+
+
+
+
+johnny = Paperboy.new('Johnny')
+
+johnny.deliver(1, 101)
+johnny.report
+johnny.deliver(1, 101)
+johnny.report
